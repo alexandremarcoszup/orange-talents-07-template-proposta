2cca793ef9dde3054ce8dd72dc067fd6486db4d8
Alexandre <alexandre.marcos@zup.com.br>
2021-08-23 17:23:18 -0300

    Adcionado a parte de health check

---
+++ src/main/java/br/com/orangetalents/proposta/security/healthcheck/HealthCheck.java
@@ -0,0 +1,22 @@
+package br.com.orangetalents.proposta.security.healthcheck;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.actuate.health.Status;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class HealthCheck implements HealthIndicator {
+
+    @Override
+    public Health health() {
+        Map<String, Object> details = new HashMap<>();
+        details.put("versão", "1.0.0");
+        details.put("descrição", "Meu primeiro Health Check customizado!");
+
+        return Health.status(Status.UP).withDetails(details).build();
+    }
+}
